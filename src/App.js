a400e38006b148e3012844a233551bc46a4e9f82
cyril-pallasigui <46484675+cyril-pallasigui@users.noreply.github.com>
2019-02-12 21:11:55 +0800

    deploy frontend to Heroku

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
       imageUrl: this.state.input
     });
     
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://mysterious-brushlands-43676.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -79,7 +79,7 @@ class App extends Component {
     })
       .then(response => response.json())
       .then(response => {
-        fetch('http://localhost:3000/image', {
+        fetch('https://mysterious-brushlands-43676.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
